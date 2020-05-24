1beb1b8e1c545f9a4ba2848ac0257a4c458b9e25
anguodong <2890815038@qq.com>
2020-05-24 22:05:02 +0800

    第一个页面

---
+++ src/components/block/an-block.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="content">
+        <div class="top">
+            <van-image
+                    round
+                    class="avatar"
+                    src="https://img.yzcdn.cn/vant/cat.jpeg"
+            />
+            <span class="name one-txt-cut">啊一啾啾</span>
+            <img class="more" src="@/assets/icon/icon_details_more.png" alt="">
+        </div>
+        <p class="text txt-cut">
+            729声工场， 创始于2005年7月29日，原名7
+            29配音组。2016年7月29日，729配音组正式
+            更名729声工场，发展成为一支 专也，729配音组正式
+            更名729声工场，发展成为一支 专也
+        </p>
+        <div class="view">
+            <div class="play">
+                <img src="@/assets/icon/icon_boardcast.png" alt="">
+            </div>
+            <div class="bottom">
+                <span class="time">01:24</span>
+                <div class="valume">
+                    <img src="@/assets/icon/icon_mute.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="tags">
+            #话题名称最长可有十二个字 >
+        </div>
+        <div class="controller">
+            <div class="tab">
+                <img src="@/assets/icon/icon_share.png" alt="">
+                <span>4568</span>
+            </div>
+            <div class="tab">
+                <img src="@/assets/icon/icon_pressed_dislike.png" alt="">
+                <span>4568</span>
+            </div>
+            <div class="tab">
+                <img src="@/assets/icon/icon_comment.png" alt="">
+                <span>4568</span>
+            </div>
+            <div class="tab">
+                <img src="@/assets/icon/icon_unpressed_thumds-up.png" alt="">
+                <span>4568</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "an-block"
+    }
+</script>
+
+<style lang="less" scoped>
+    .content{
+        width:345px;
+        padding-bottom: 12px;
+        background:rgba(255,255,255,1);
+        box-shadow:0px 2px 5px 0px rgba(0,0,1,0.3);
+        border-radius:5px;
+        margin:15px;
+    .top{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        padding-top: 10px;
+    .avatar{
+        width:25px;
+        height:25px;
+        margin-left: 10px;
+        margin-right: 11px;
+    }
+    .name{
+        font-size:15px;
+        font-family:Source Han Sans CN;
+        font-weight:400;
+        color:rgba(0,0,0,1);
+        width: 250px
+    }
+    .more{
+        width:15px;
+        height:3px;
+        margin-left: auto;
+        margin-right: 15px;
+    }
+    }
+    .text{
+        width:314px;
+        font-size:16px;
+        font-family:Source Han Sans CN;
+        font-weight:400;
+        color:rgba(0,0,0,1);
+        line-height:24px;
+        margin:13px 16px;
+        -webkit-line-clamp: 3;
+    }
+    .view{
+        width:320px;
+        border-radius:10px;
+        height: 215px;
+        box-sizing: border-box;
+        background: tan;
+        margin:0 auto 12px auto;
+        padding-top: 93px;
+        padding-bottom: 8px;
+    .play{
+        width:30px;
+        height:30px;
+        margin:0 auto 72px auto;
+    img{
+        width: 100%;
+        height: 100%;
+    }
+    }
+    .bottom{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    .time{
+        font-size: 12px;
+        color: white;
+        margin-left: 10px;
+    }
+    .valume{
+        margin-right: 10px;
+        width:17px;
+        height:13px;
+    img{
+        width: 100%;
+        height: 100%;
+    }
+    }
+    }
+    }
+    .tags{
+        background:rgba(0,0,0,1);
+        opacity:0.1;
+        border-radius:5px;
+        display: inline-block;
+        color: #999999;
+        padding: 4px 8px;
+        margin-left: 15px;
+        margin-bottom: 13px;
+    }
+    .controller{
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 16px;
+        box-sizing: border-box;
+    .tab{
+        display: flex;
+        align-items: center;
+        margin-left: 24px;
+    img{
+        width:19px;
+        height:19px;
+    }
+    span{
+        font-size: 12px;
+        color: #BBBBBB;
+        margin-left: 6px;
+    }
+    }
+    }
+    }
+</style>
---
+++ src/views/Home/home.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="home">
+       <div class="header">
+           <van-image
+                   round
+                   class="avatar"
+                   src="https://img.yzcdn.cn/vant/cat.jpeg"
+           />
+           <van-tabs :border="false" background="transparent" color="#FFA70D" class="tabs" v-model="active" title-active-color="#FFA70D" title-inactive-color="#999999">
+               <van-tab title="关注" />
+               <van-tab title="推荐" />
+               <van-tab title="话题" />
+           </van-tabs>
+           <van-icon size="24" class="search" name="search" />
+       </div>
+        <van-pull-refresh class="box" v-model="refreshing" @refresh="onRefresh">
+            <van-list
+                    v-model="loading"
+                    :finished="finished"
+                    finished-text="没有更多了"
+                    @load="onLoad"
+            >
+                <an-block v-for="(item,index) in list" :key="index"/>
+            </van-list>
+        </van-pull-refresh>
+
+    </div>
+</template>
+
+<script>
+    import AnBlock from "@/components/block/an-block";
+    export default {
+        name: "home",
+        components: {AnBlock},
+        data(){
+            return {
+                active:'',
+                list: [],
+                loading: false,
+                finished: false,
+                refreshing: false,
+            }
+        },
+        methods:{
+            onLoad() {
+                setTimeout(() => {
+                    if (this.refreshing) {
+                        this.list = [];
+                        this.refreshing = false;
+                    }
+
+                    for (let i = 0; i < 10; i++) {
+                        this.list.push(this.list.length + 1);
+                    }
+                    this.loading = false;
+
+                    if (this.list.length >= 40) {
+                        this.finished = true;
+                    }
+                }, 1000);
+            },
+            onRefresh() {
+                // 清空列表数据
+                this.finished = false;
+
+                // 重新加载数据
+                // 将 loading 设置为 true，表示处于加载状态
+                this.loading = true;
+                this.onLoad();
+            },
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+.home{
+    height: 100%;
+    background: url("../../assets/icon/bg.png");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    padding-top: 8px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    .header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 50px;
+        overflow: hidden;
+        flex-shrink: 0;
+        .avatar{
+            width:35px;
+            height:35px;
+            margin-left: 15px;
+        }
+        .tabs{
+            width: 220px;
+            margin:auto;
+            /deep/ .van-tab{
+                font-size: 18px;
+            }
+            /deep/ .van-tab--active{
+                font-size: 21px;
+            }
+        }
+        .search{
+            margin-left: auto;
+            margin-right: 15px;
+        }
+    }
+    .box{
+        flex:1;
+        overflow-y: auto;
+    }
+
+}
+</style>
